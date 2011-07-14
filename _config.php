8a342538ed8f6ff381c08ec5034ea6a34df68b9f
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-07-14 05:30:02 +0000

    adding basic files to ecommerce_product_tags
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_product_tags/trunk@1314 950b781c-c544-efab-2f99-90083ee69683

---
+++ _config.php
@@ -0,0 +1,12 @@
+<?php
+
+
+/**
+ * developed by www.sunnysideup.co.nz
+ * author: Nicolaas - modules [at] sunnysideup.co.nz
+**/
+Object::add_extension("Product","EcommerceProductTag");
+
+//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+//===================---------------- START ecommerce_product_tag MODULE ----------------===================
+//===================---------------- END ecommerce_product_tag MODULE ----------------===================
