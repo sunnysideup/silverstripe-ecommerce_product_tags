c4932aafa03e1efba3db07caf2d7728c3b83ac00
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2011-09-27 11:37:37 +0000

    ecommerce_product_tags/ cms improvements
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_product_tags/trunk@1532 950b781c-c544-efab-2f99-90083ee69683

--- code/dod/EcommerceProductTagProductDecorator.php
+++ code/dod/EcommerceProductTagProductDecorator.php
@@ -15,10 +15,31 @@ class EcommerceProductTagProductDecorator extends DataObjectDecorator {
 		$dos = DataObject::get("EcommerceProductTag");
 		if($dos && $this->owner->ID) {
 			$dosArray = $dos->toDropDownMap();
-			$fields->addFieldToTab("Root.Content.Tags", new CheckboxSetField("EcommerceProductTags", "Select Relevant Tags", $dosArray));
+			$fields->addFieldsToTab(
+				"Root.Content.Tags",
+				array(
+					new CheckboxSetField("EcommerceProductTags", "Select Relevant Tags", $dosArray),
+					new TextField("AddATag", "Add a Tag")
+				)
+			);
 		}
 	}
 
+	function onBeforeWrite() {
+		if(isset($_REQUEST["AddATag"])) {
+			$name = Convert::raw2sql($_REQUEST["AddATag"]);
+			if($name) {
+				if(!DataObject::get_one("EcommerceProductTag", "\"Title\" = '$name' OR \"Code\" = '$name'")) {
+					$obj = new EcommerceProductTag();
+					$obj->Title = $name;
+					$obj->Code = $name;
+					$obj->write;
+					$obj->Products()->add($obj);
+					$obj->write();
+				}
+			}
+		}
+	}
 
-} 
- 
+}
+
