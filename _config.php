ae1d17dd2885b7f47826e1aad6ef871f302b640f
nfrancken@gmail.com <nfrancken@gmail.com@950b781c-c544-efab-2f99-90083ee69683>
2014-04-09 04:40:31 +0000

    UPGRADE 3.1
    git-svn-id: https://silverstripe-ecommerce.googlecode.com/svn/modules/ecommerce_product_tags/trunk@4930 950b781c-c544-efab-2f99-90083ee69683

--- _config.php
+++ _config.php
@@ -2,20 +2,13 @@
 
 
 /**
- * developed by www.sunnysideup.co.nz
  * author: Nicolaas - modules [at] sunnysideup.co.nz
-**/
 
-//copy the lines between the START AND END line to your /mysite/_config.php file and choose the right settings
+# ADD TO ECOMMERCE.YAML:
 
-//===================---------------- START ecommerce_product_tag MODULE ----------------===================
-//Object::add_extension("Product","EcommerceProductTagProductDecorator");
-/**
- * ADD TO ECOMMERCE.YAML:
 ProductsAndGroupsModelAdmin:
 	managed_modules: [
 		...
 		EcommerceProductTag
 	]
 */
-//===================---------------- END ecommerce_product_tag MODULE ----------------===================
